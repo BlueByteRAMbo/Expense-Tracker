32d5c572da72a20784b646610353dca469db1b8e
LACHHANI Nirmal OBS/IZ <lachhanijatin@gmail.com>
2026-03-14 14:35:45 +0530

    Project structure Ready and Header Component loaded

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    return (
+        <header>
+            <h1>Expense Tracker</h1>
+            <p>A web app to manage and track your expenses</p>
+        </header>
+    )
+}
+
+export default Header
